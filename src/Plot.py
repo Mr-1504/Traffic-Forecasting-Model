f4f24233e5e31e2f29f86b04ce9687ac2c1f745b
Mr-1504 <truongvan.minh1504@gmail.com>
2025-01-02 00:23:34 +0700

    Update Plot.py

--- src/Plot.py
+++ src/Plot.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 data = pd.read_csv('../resource/train27303.csv')
 data['timestamp'] = pd.to_datetime(data['timestamp'])
 data.sort_values('timestamp', inplace=True)
+data = data[(data['timestamp'] <= '2015-11-25')]
 print(data)
 plt.figure(figsize=(20, 10))
 plt.plot(data['timestamp'], data['hourly_traffic_count'], label='Traffic Count', color='green')
@@ -13,8 +15,9 @@ plt.legend()
 plt.savefig('hourly traffic count overtime.png')
 plt.close()
 
-test_data = data[(data['timestamp'] >= '2015-12-27') & (data['timestamp'] <= '2015-12-30')]
-
+train_size = int(len(data) * 0.8)
+train_data = data[:train_size]
+test_data = data[train_size:]
 plt.figure(figsize=(20, 10))
 plt.plot(test_data['timestamp'], test_data['hourly_traffic_count'], label='Traffic Count', color='green')
 plt.ylabel('Traffic Count')
